4540e1da45f88f7bc7b14e221c70211fa3730de6
dev-assassin <alexandra.uefa@gmail.com>
2020-05-12 03:03:14 +0300

    add & change system files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  application:
+  {
+    name: 'angular-starter',
+    angular: 'Angular 9.1.4',
+    bootstrap: 'Bootstrap 4.4.1',
+    fontawesome: 'Font Awesome 5.13.0',
+  }
 };
