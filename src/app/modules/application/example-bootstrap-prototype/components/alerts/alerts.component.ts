557a3187ec1b3cc108e8d3fe61b8a9ad4bc9124d
dev-assassin <alexandra.uefa@gmail.com>
2020-05-13 15:44:53 +0300

    update alerts comp and add badge comp

--- src/app/modules/application/example-bootstrap-prototype/components/alerts/alerts.component.ts
+++ src/app/modules/application/example-bootstrap-prototype/components/alerts/alerts.component.ts
@@ -12,6 +12,6 @@ export class AlertComponent implements OnInit {
     constructor() {}
 
     ngOnInit(): void {
-        
+
     }
 }
--- src/app/modules/application/example-bootstrap-prototype/example-bootstrap-prototype-routing.module.ts
+++ src/app/modules/application/example-bootstrap-prototype/example-bootstrap-prototype-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import { ExampleBootstrapPrototypeComponent } from './example-bootstrap-prototype.component';
 import { AlertComponent } from './components/alerts/alerts.component';
+import { BadgeComponent } from './components/badge/badge.component';
 
 const routes: Routes = [
     {path: '', component: ExampleBootstrapPrototypeComponent, children: [
         {path: 'alerts', component: AlertComponent},
+        {path: 'badge', component: BadgeComponent},
         {path: '', redirectTo: '/bootstrap-prototype/alerts', pathMatch: 'full'}
     ]}
 ]
