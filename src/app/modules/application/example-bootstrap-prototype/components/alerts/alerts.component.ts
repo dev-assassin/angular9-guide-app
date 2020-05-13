3841cb70334bce9a197c6f84f4c60ac19881f66d
dev-assassin <alexandra.uefa@gmail.com>
2020-05-13 13:29:29 +0300

    add alerts comp

---
+++ src/app/modules/application/example-bootstrap-prototype/components/alerts/alerts.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+    selector: 'app-alerts',
+    templateUrl: './alerts.component.html',
+    styleUrls: ['./alerts.component.css']
+})
+
+export class AlertComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
+}
