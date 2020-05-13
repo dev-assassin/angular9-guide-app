f7a6100d1ae4638df08556b82734a94196270c08
dev-assassin <alexandra.uefa@gmail.com>
2020-05-13 21:58:39 +0300

    add blockquotes comp

---
+++ src/app/modules/application/example-bootstrap-prototype/components/blockquotes/blockquotes.components.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-blockquotes',
+    templateUrl: './blockquotes.component.html',
+    styleUrls: ['./blockquotes.component.css']
+})
+
+export class BlockquotesComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit():void {
+
+    }
+}
--- src/app/modules/application/example-bootstrap-prototype/example-bootstrap-prototype-routing.module.ts
+++ src/app/modules/application/example-bootstrap-prototype/example-bootstrap-prototype-routing.module.ts
@@ -3,11 +3,13 @@ import {Routes, RouterModule} from '@angular/router';
 import { ExampleBootstrapPrototypeComponent } from './example-bootstrap-prototype.component';
 import { AlertsComponent } from './components/alerts/alerts.component';
 import { BadgeComponent } from './components/badge/badge.component';
+import { BlockquotesComponent } from './components/blockquotes/blockquotes.components';
 
 const routes: Routes = [
     {path: '', component: ExampleBootstrapPrototypeComponent, children: [
         {path: 'alerts', component: AlertsComponent},
         {path: 'badge', component: BadgeComponent},
+        {path: 'blockquotes', component: BlockquotesComponent},
         {path: '', redirectTo: '/bootstrap-prototype/alerts', pathMatch: 'full'},
         {path: '**', component: AlertsComponent}
     ]}
