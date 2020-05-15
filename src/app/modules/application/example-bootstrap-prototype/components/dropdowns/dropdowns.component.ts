e9abf096cf5da08691af739458250058bd4ea6a7
dev-assassin <alexandra.uefa@gmail.com>
2020-05-15 07:32:44 +0300

    update collapse html&add dropdown comp

---
+++ src/app/modules/application/example-bootstrap-prototype/components/dropdowns/dropdowns.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdowns',
+  templateUrl: './dropdowns.component.html',
+  styleUrls: ['./dropdowns.component.css']
+})
+export class DropdownsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
