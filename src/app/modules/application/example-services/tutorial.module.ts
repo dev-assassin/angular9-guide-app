2983d4fb9fde3724d16165d595ac867daf80fbd0
dev-assassin <alexandra.uefa@gmail.com>
2020-06-01 20:24:48 +0300

    add example-services comp

---
+++ src/app/modules/application/example-services/tutorial.module.ts
@@ -0,0 +1,23 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// import { TutorialRoutingModule } from './tutorial-routing.module';
+import { TutorialComponent } from './tutorial.component';
+
+// import { SafePipe } from './safe.pipe';
+
+@NgModule({
+    declarations: [
+        TutorialComponent,
+        // SafePipe,
+    ],
+    imports: [
+        CommonModule,
+        // TutorialRoutingModule
+    ],
+    exports: [
+        TutorialComponent,
+    ],
+})
+export class TutorialModule {}
