eb7d79a657828c2dace719587d81f91c9c7e208c
dev-assassin <alexandra.uefa@gmail.com>
2020-06-02 19:09:12 +0300

    add items component

---
+++ src/app/modules/application/items/items.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ItemsComponent } from './items.component';
+// import { ItemsRoutingModule } from './items-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ItemsService } from './items.service';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        // ItemsRoutingModule,
+        HttpClientModule
+    ],
+    exports: [
+        ItemsComponent
+    ],
+    declarations: [
+        ItemsComponent
+    ],
+    providers: [ItemsService
+    ],
+})
+export class ItemsModule { }
